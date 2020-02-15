6bc091baba9d1a8091de68fcac15fa5d8e99d94d
Julio Hurtado (Juliens) <jhurtado@mmorona.gob.ec>
2020-02-15 17:23:21 -0500

    Change character detail

--- characters/admin.py
+++ characters/admin.py
@@ -4,4 +4,9 @@ from django.contrib import admin
 from .models import Character
 
 
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = "name", "nick", "range", "user"
+    list_editable = ("user",)
+
+
 admin.site.register(Character)
