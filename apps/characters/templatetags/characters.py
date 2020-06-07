6449de06a2844f062382e6cc19e852f078ffb877
Julio Hurtado (Juliens) <jhurtado@mmorona.gob.ec>
2020-06-06 22:54:11 -0500

    :✨ Add bbcode copy icon

--- apps/characters/templatetags/characters.py
+++ apps/characters/templatetags/characters.py
@@ -14,3 +14,12 @@ def icon(character, achievement):
     if filtered_achievement.exists():
         return {"css": "", "legend": "Obtenido"}
     return {"css": "opacity:25%;", "legend": "No obtenido"}
+
+
+@register.simple_tag
+def icon_bbcode(character, achievement):
+
+    filtered_achievement = character.achievements.filter(pk=achievement.pk)
+    if filtered_achievement.exists():
+        return {"url": achievement.icon_principal.url, "legend": "Obtenido"}
+    return {"url": achievement.icon_secondary.url, "legend": "No obtenido"}
